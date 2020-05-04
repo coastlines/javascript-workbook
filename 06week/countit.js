223a794646d873bf162e988029089c7168a9324a
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-05-03 20:27:15 -0500

    count it

--- 06week/countit.js
+++ 06week/countit.js
@@ -1,3 +1,4 @@
+// CONSOLE VERSION
 // let string = ` a sammple string 345 na7not a word 
 // ok 
 
@@ -14,47 +15,39 @@
 //   }, {});
 // }
 
-const countButton = document.getElementById('countButton');
+//WEB VERSION
 
 // submit string to count
+const countButton = document.getElementById('countButton');
 
 countButton.addEventListener('click', function() {
   let textToCount = document.getElementById('toCount').value;
-  console.log('clicked button')
   let displayText = countIt(textToCount);
   addText(displayText);
 })
 
+// count number of appearances of each character after removing white spaces and non-alphabetic characters
 function countIt(textToCount) {
   let str = textToCount.replace(/[^a-z]/igm,"");
-  console.log(textToCount);
-
   let result = str.split('').reduce((count, character) => {
-    console.log("looking at character", character);
     count[character] = (count[character] || 0) + 1;
     return count;
-    //document.getElementById("countText").innerHTML = count;
   },{})
   return result;
 }
 
+// sort results alphabetically and create text to display
 function addText(result) {
-  //document.getElementById("countText").innerHTML += Object.keys(count) + Object.values(count);
   let sortedResult = Object.keys(result).sort().reduce((r, k) => (r[k] = result[k], r), {});
-  Object.entries(sortedResult).forEach(([key,value, i]) => {
-        document.getElementById("countText").innerHTML += `${key}: ${value} <br>`;
+  Object.entries(sortedResult).forEach(([key,value]) => {
+      document.getElementById("countText").innerHTML += `${key}: ${value} <br>`;
     });
-  //document.getElementById("countText").style.whiteSpace = "pre-line";
 }
-  // reset button
+
+// reset button
 const resetButton = document.getElementById('resetButton');
 
 resetButton.addEventListener('click', function() {
   document.getElementById('toCount').value = '';
-  document.getElementById("countText-1").innerHTML = '';
-  document.getElementById("countText-2").innerHTML = '';
-  console.log('cleared text');
+  document.getElementById('countText').innerHTML = '';
 })
-
-// count = (`${count.key}: ${count.value}`);
- //displayCount = JSON.stringify(count)
